2330457c910fbc10bd13d0aec982ee65e0a2c497
workingjubilee <workingjubilee@gmail.com>
2019-04-02 13:32:52 -0700

    Guided project completion. Maybe.

--- api/server.js
+++ api/server.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
+const configuredKnex = require('../database/dbConfig.js');
 
 const server = express();
+
+const sessionConfig = {
+  name: 'monster', // defaults to sid
+  secret: 'keep it secret, keep it safe!',
+  cookie: {
+    maxAge: 1000 * 60 * 10, // milliseconds
+    secure: false, // use cookie over https
+    httpOnly: true, // false means JS can access the cookie on the client
+  },
+  resave: false, // avoid recreating unchanged sessions
+  saveUninitialized: false, // GDPR compliance
+  store: new KnexSessionStore({
+    knex: configuredKnex,
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true, 
+    clearInterval: 1000 * 60 * 30,
+  }),
+};
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 
+
 const Users = require('../users/users-model.js');
 
 // for endpoints beginning with /api/auth
@@ -25,6 +26,9 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+
+        req.session.user = user;
+
         res.status(200).json({
           message: `Welcome ${user.username}!`,
         });
@@ -36,5 +40,19 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+router.get('/logout', (req,res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.status(500).json({ message: "Trapped!" });
+      } else {
+        res.status(200).json({ message: "Byeee!" })
+      }
+    })
+  } else {
+    res.status(200).json({ message: "Byeee!" })
+  }
+})
 
 module.exports = router;
